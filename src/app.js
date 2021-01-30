21f5523e803c0f545477ea6c4ba8bb95a3c61bda
tobiloba <o.alaran@concept-nova.com>
2021-01-30 22:19:12 +0100

    Import body-parser in app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { json } from 'body-parser';
-import validateRuleRouter from './routes/validate-rule';
+import { json, urlencoded } from 'body-parser';
+import validateRuleRouter from './routes/rule-validator';
 import errorHandler from './middlewares/error-handler';
 
 const app = express();
 
 app.use(json());
+app.use(urlencoded({ extended: true }));
 app.use(validateRuleRouter);
 
 app.use(errorHandler);
