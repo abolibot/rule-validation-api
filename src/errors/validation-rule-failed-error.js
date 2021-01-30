8c07ed011a64bc035979b08eca0e382b810f61f3
tobiloba <o.alaran@concept-nova.com>
2021-01-30 22:22:51 +0100

    Remove ValidationRuleFailed custom error class

--- src/errors/validation-rule-failed-error.js
+++
@@ -1,9 +0,0 @@
-class ValidationRuleFailedError extends Error {
-  constructor() {
-    super();
-
-    Object.setPrototypeOf(this, ValidationRuleFailedError.prototype);
-  }
-}
-
-export default ValidationRuleFailedError;
