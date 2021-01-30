693d4f0dbf9b41e485a5bd7cd1a0ab808600adeb
tobiloba <o.alaran@concept-nova.com>
2021-01-30 10:57:01 +0100

    Complete base route handler

--- src/routes/validate-rule.js
+++ src/routes/validate-rule.js
@@ -4,7 +4,15 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.status(200).json({
-    message: 'hello world!'
+    message: 'My Rule-Validation API',
+    status: 'success',
+    data: {
+      name: 'Oluwatobi Alaran',
+      github: '@abolibot',
+      email: 'alarantobiloba@gmail.com',
+      mobile: '08175520794',
+      twitter: '@__tobiMac'
+    }
   });
 });
 
